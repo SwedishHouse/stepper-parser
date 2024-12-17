0a932a82f0093240a64221898b1ba7c70a85a5d6
Вадим Мещеряков <swedish2015@yandex.ru>
2024-12-17 10:39:17 +0400

    tests added

---
+++ experience/multiple_gcmd.py
@@ -0,0 +1,80 @@
+import re
+
+
+class Gcode:
+
+    def __init__(self, name: str, params: dict[str, float] | None) -> None:
+        self.name = name
+        self.params = params
+
+    @property
+    def get_param(self) -> str:
+        if self.params:
+            return ' '.join(key + str(val) for key, val in self.params.items())
+        return ''
+
+    @property
+    def get_name(self) -> str:
+        return self.name
+
+    def get_cmd(self):
+        return self.name + ' ' * bool(self.get_param) + self.get_param
+
+class GCodeParsed:
+
+    def __init__(self, line: str) -> None:
+        gcode = line.split()
+        self.cmd = gcode[0]
+        self.params = self.convert_to_dict(gcode[1::])
+
+    def get(self):
+        return {"cmd": self.cmd, "params": self.params}
+
+    @staticmethod
+    def convert_to_dict(coordinate_list):
+        result = {}
+        for item in coordinate_list:
+            match = re.match(r"([A-Z]+)([-+]?\d+)", item)
+            if match:
+                key = match.group(1)
+                value = int(match.group(2))
+                result[key] = value
+        return result
+
+
+class GCodeSplitter:
+
+    def __init__(self) -> None:
+        self.pattern = r'([GMSFTDH]\d+\.?\d*)|([F]\d+\.?\d*)'
+
+    def parse_gcode_line(self, line):
+        # Регулярное выражение для поиска команд и их параметров
+        pattern = r'([GMSFTDH]\d+\.?\d*)|([XYZIJKR]\d+\.?\d*)'
+        matches = re.findall(pattern, line)
+
+        # Объединяем команды и параметры в один список
+        commands = []
+        current_command = None
+
+        for match in matches:
+            command = match[0] if match[0] else match[1]
+
+            # Если это новая команда, добавляем ее в список
+            if re.match(r'[GMSFTDH]', command):
+                if current_command:
+                    commands.append(current_command)
+                current_command = command
+            else:
+                # Если это параметр, добавляем его к текущей команде
+                if current_command:
+                    current_command += ' ' + command
+
+        # Добавляем последнюю команду
+        if current_command:
+            commands.append(current_command)
+
+        return commands
+
+
+if __name__ == '__main__':
+    pass
---
+++ test-gcode-multiple-line.py
@@ -0,0 +1,59 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from experience.multiple_gcmd import Gcode, GCodeSplitter, GCodeParsed
+import json
+import pytest
+
+
+def test_g0():
+    x_coord = 15.2
+    gcode = Gcode('G00', {'X': x_coord})
+
+    assert f'G00 X{x_coord}' == gcode.get_cmd()
+
+
+def test_g1():
+    x_coord = 15.2
+    y_coord = 42.3
+    z_coord = 12.4
+    gcode = Gcode('G01', {'X': x_coord, 'Y': y_coord, 'Z': z_coord})
+
+    assert f'G01 X{x_coord} Y{y_coord} Z{z_coord}' == gcode.get_cmd()
+
+
+def test_g40():
+    gcode = Gcode('G40', {})
+
+    assert f'G40' == gcode.get_cmd()
+
+def test_splitter():
+    splitter = GCodeSplitter()
+    assert ['G40', 'G01 X100 Y52.4', 'G54', 'F100'] == splitter.parse_gcode_line('G40 G01 X100 Y52.4 G54 F100')
+
+def test_gcode_parsed_single_string():
+    gcode = GCodeParsed("G1 X5 Y15")
+    res = {"cmd": "G1", "params": {"X": 5, "Y": 15}}
+    assert res == gcode.get()
+
+# Загрузка тестовых данных из файла
+@pytest.fixture
+def test_data():
+    data_file_path = "D:\\GitHub\\stepper-parser\\gcode-files\\test_gcode_data.json"
+    with open(data_file_path, 'r', encoding='utf-8') as f:
+        return json.load(f)
+
+def test_process_data(test_data):
+    for case in test_data:
+
+        input_data = case['input']
+        expected_result = case['expected']
+        gcode = GCodeParsed(input_data)
+        assert gcode.get() == expected_result
+
+
+
+if __name__ == "__main__":
+    # splitter = GCodeSplitter()
+    # splitter.parse_gcode_line('G40 G01 X100 G54')
+    print(os.path.join(os.path.dirname(__file__)))
